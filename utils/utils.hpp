8dba3f51c5084e9bf523901d76f325a5a07d074e
rnitta <rnitta@student.42tokyo.jp>
2022-11-16 10:51:46 +0900

    add prototype definition

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -2,13 +2,15 @@
 # define UTILS_HPP
 
 # include <iostream>
+# include <memory>    // shared_ptr
+# include <array>
 
 namespace rnitta
 {
 
 
 std::string trim( const std::string src, const char *trimCharList = " \t\v\r\n");
-
+bool ExecCmd(const char* cmd, std::string& stdOut, int& exitCode);
 
 }	// namespace rnitta
 
