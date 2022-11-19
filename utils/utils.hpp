ee9e5837e38780d23a7547f26ac0aba20607d7b8
rnitta <rnitta@student.42tokyo.jp>
2022-11-19 11:52:00 +0900

    define SUCCESS=0, ERROR=-1

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -8,6 +8,8 @@
 namespace rnitta
 {
 
+# define SUCCESS 0
+# define ERROR -1
 
 std::string trim( const std::string src, const char *trimCharList = " \t\v\r\n");
 bool ExecCmd(const char* cmd, std::string& stdOut, int& exitCode);
